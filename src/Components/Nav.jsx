b89e44832a17fdcf58dd0091cfc6c4fe2f67ad0e
inyangj <inyangjohnson01@gmail.com>
2023-09-04 11:10:07 +0100

    hot fix

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Nav = () => {
     return(
-        <div className='flex justify-between items-center bg-[rgba(225,225,225,0.1)] p-2 rounded'>
+        <div className='flex justify-between items-center  p-2 rounded'>
             <img src="/cover1.png" alt="icon" />
 
-                <p>Blog</p>
-                <p>Pricing</p>
 
             
         </div>
